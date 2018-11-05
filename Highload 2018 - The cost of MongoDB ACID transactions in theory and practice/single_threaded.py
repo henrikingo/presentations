75901eb0943f1b742b4e5dbd6208997983c811cb
Henrik Ingo <henrik.ingo@mongodb.com>
2018-11-05 14:17:52 +0200

    Small edits from review

--- Highload 2018 - The cost of MongoDB ACID transactions in theory and practice/single_threaded.py
+++ Highload 2018 - The cost of MongoDB ACID transactions in theory and practice/single_threaded.py
@@ -6,12 +6,9 @@ config = {
 #   Test config
     'test_name': 'Single threaded tests',
     'iterations': 100,
-#   DB config
-    'dbhost': 'localhost',
-    'dbport': '27017',
-    'dbname': 'test',
-#    'dbuser': '',
-#    'dbpassword': '',
+    'mongodb_url': 'mongodb+srv://dsi-user:oyKDMvndicnuIz8H@hltest-5thdf.mongodb-dev.net/test?retryWrites=true'
+    #'mongodb_url': 'mongodb://10.2.0.200/test?retryWrites=true'
+    #'mongodb_url': 'mongodb://localhost/test?retryWrites=true'
 }
 
 
@@ -25,6 +22,9 @@ import sys
 import time
 import pprint
 import pymongo
+from pymongo.write_concern import WriteConcern
+from pymongo.read_concern import ReadConcern
+from pymongo.read_preferences import ReadPreference
 
 #### functions to test
 # Note that we expect caller has set global variable `db`
@@ -35,16 +35,51 @@ def foo(arg):
 def update():
     db.hltest.update_one( {'_id': 1}, {'$inc': {'n': 1}} )
 
+def readwrite(session=None):
+    amount = random.randint(-100, 100)
+    result1 = db.hltest.find_one( { '_id': 1 }, session=session )
+    db.hltest.update_one( {'_id': 1}, {'$inc': {'n': -amount}}, session=session )
+    result2 = db.hltest.find_one( { '_id': 2 }, session=session )
+    db.hltest.update_one( {'_id': 2}, {'$inc': {'n': amount}}, session=session )
+    cursor = db.hltest.aggregate( [ { '$group': { '_id': 'foo', 'total' : { '$sum': '$n' } } } ], session=session )
+    result = cursor.next()
+    if result['total'] != 200:
+        print "ERROR: sum total = %s" % result['total']
+
+def rw_session():
+    with db.client.start_session(causal_consistency=True) as session:
+        readwrite(session=session)
+
+def rw_trx(write_concern=None, read_concern=None, read_preference=None):
+    with db.client.start_session(causal_consistency=True) as session:
+        session.start_transaction(write_concern=write_concern, read_concern=read_concern, read_preference=read_preference)
+        readwrite(session=session)
+        session.commit_transaction()
+
 # (test_function, *args, write_concern, read_concern, read_preference)
+#tests = [
+#(update, [], (WriteConcern(w=0), None, None)),
+#(update, [], (WriteConcern(w=1), None, None)),
+#(update, [], (WriteConcern(w=2), None, None)),
+#(update, [], (WriteConcern(w=3), None, None)),
+#(update, [], (WriteConcern(j=True), None, None)),
+#(update, [], (WriteConcern(w=2, j=True), None, None)),
+#(update, [], (WriteConcern(w="majority", j=False), None, None)),
+#(update, [], (WriteConcern(w="majority", j=True), None, None)),
+#]
+
 tests = [
-(update, [], pymongo.write_concern.WriteConcern(w=0)),
-(update, [], pymongo.write_concern.WriteConcern(w=1)),
-(update, [], pymongo.write_concern.WriteConcern(w=2)),
-(update, [], pymongo.write_concern.WriteConcern(w=3)),
-(update, [], pymongo.write_concern.WriteConcern(j=True)),
-(update, [], pymongo.write_concern.WriteConcern(w=2, j=True)),
-(update, [], pymongo.write_concern.WriteConcern(w="majority", j=False)),
-(update, [], pymongo.write_concern.WriteConcern(w="majority", j=True)),
+(readwrite, [], (WriteConcern(w=0), ReadConcern(level="local"), ReadPreference.PRIMARY)),
+(readwrite, [], (WriteConcern(w=1), ReadConcern(level="local"), ReadPreference.PRIMARY)),
+(readwrite, [], (WriteConcern(w=1), ReadConcern(level="local"), ReadPreference.SECONDARY)),
+(readwrite, [], (WriteConcern(w="majority"), ReadConcern(level="majority"), ReadPreference.PRIMARY)),
+(readwrite, [], (WriteConcern(w="majority"), ReadConcern(level="majority"), ReadPreference.SECONDARY)),
+(readwrite, [], (WriteConcern(w="majority"), ReadConcern(level="linearizable"), ReadPreference.PRIMARY)),
+(rw_session, [], (WriteConcern(w="majority"), ReadConcern(level="majority"), ReadPreference.PRIMARY)),
+(rw_session, [], (WriteConcern(w="majority"), ReadConcern(level="majority"), ReadPreference.SECONDARY)),
+(rw_trx, [], (WriteConcern(w="majority"), ReadConcern(level="snapshot"), ReadPreference.PRIMARY)),
+(rw_trx, [WriteConcern(w="majority"), ReadConcern(level="snapshot"), ReadPreference.PRIMARY], (WriteConcern(w="majority"), ReadConcern(level="snapshot"), ReadPreference.PRIMARY)),
+#(rw_trx, [WriteConcern(w="majority"), ReadConcern(level="snapshot"), ReadPreference.SECONDARY], (WriteConcern(w="majority"), ReadConcern(level="snapshot"), ReadPreference.SECONDARY)),
 ]
 
 #### MongoDB
@@ -63,16 +98,21 @@ def get_db_info(c) :
   return c
 
 
-def get_db(write_concern=None) :
-    client = pymongo.MongoClient( "mongodb://%s:%s/" % (config["dbhost"], config["dbport"]) )
-    return pymongo.database.Database(client=client, name=config["dbname"], write_concern=write_concern)
+def get_db(db_opts=None) :
+    write_concern=db_opts[0] if db_opts else None
+    read_concern=db_opts[1] if db_opts else None
+    read_preference=db_opts[2] if db_opts else None
+
+    client = pymongo.MongoClient( config["mongodb_url"] )
+    return pymongo.database.Database(client=client, name="test", write_concern=write_concern, read_concern=read_concern, read_preference=read_preference)
 
 # Test initialization
 def db_init() :
   db = get_db()
   db.hltest.drop()
   # These tests do finds and updates on a single record
-  db.hltest.insert( {'_id': 1, 'n': 0} )
+  db.hltest.insert( {'_id': 1, 'n': 100} )
+  db.hltest.insert( {'_id': 2, 'n': 100} )
 
 
 #### test facilities
@@ -103,13 +143,14 @@ def print_stats(timings, i):
     print "Range (max-min/median):\t%s" % ((numpy.max(timings) - numpy.min(timings))/numpy.median(timings))
     print "Min            :\t%s" % numpy.min(timings)
     print "Median (50%%)   :\t%s" % numpy.median(timings)
-    print "90%% percentile :\t%s" % numpy.percentile(timings, 0.9)
-    print "95%% percentile :\t%s" % numpy.percentile(timings, 0.95) 
-    print "98%% percentile :\t%s" % numpy.percentile(timings, 0.98) 
-    print "99%% percentile :\t%s" % numpy.percentile(timings, 0.99) 
+    print "90%% percentile :\t%s" % numpy.percentile(timings, 90)
+    print "95%% percentile :\t%s" % numpy.percentile(timings, 95) 
+    print "98%% percentile :\t%s" % numpy.percentile(timings, 98) 
+    print "99%% percentile :\t%s" % numpy.percentile(timings, 99) 
     print "Max            :\t%s" % numpy.max(timings)
     print
-    print ", ".join(map(str, timings))
+    if config['iterations'] <= 100:
+        print ", ".join(map(str, timings))
     sys.stdout.flush()
 
 def generate_graphs(timings, i):
@@ -133,9 +174,9 @@ def main():
     pprint.pprint(config)
 
     counter = 0
-    for f, args, w in tests:
+    for f, args, db_opts in tests:
         global db
-        db = get_db(w)
+        db = get_db(db_opts)
         db_init()
         timings = loop(f, *args)
         report(timings, counter)
